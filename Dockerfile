344ef725daf657f92e16ef047e286635eb30e500
Nhan Nguyen <nhan.nguyen@wizeline.com>
2020-05-19 14:56:08 +0700

    Commit bash script to change whole docker compose file

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM nginx:latest
 COPY ./build /var/project
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY run.sh /config/run.sh
+COPY .env /config/.env
 RUN chmod +x /config/run.sh
 EXPOSE 80 8085
 #ENTRYPOINT ["nginx","-g","daemon off;"]
-ENTRYPOINT ["/bin/sh", "/config/run.sh"]
+ENTRYPOINT ["/bin/bash", "/config/run.sh"]
--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 cp /var/project/js/config.template.js /var/project/js/config.js
-sed -i "s|{{REACT_APP_HELLO_STRING}}|$REACT_APP_HELLO_STRING|g" /var/project/js/config.js
-sed -i "s|{{REACT_APP_BASE_URL}}|$REACT_APP_BASE_URL|g" /var/project/js/config.js
+touch /var/project/js/config.js
+while IFS='' read -r line || [ "$line" ];
+do
+  # Split env variables by character `=`
+  if printf '%s\n' "$line" | grep -q -e '=';
+  then
+    env_name=$(printf '%s\n' "$line" | sed -e 's/=.*//')
+    env_value=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+  fi
+  # get current system variable
+  value=$(printf '%s\n' "${!env_name}")
+  echo "$env_name: \"$env_value\" , $value"
+  # replace in config file
+  sed -i "s|{{$env_name}}|$value|g" /var/project/js/config.js
+done < /config/.env
 echo 'after update'
 cat /var/project/js/config.js
 nginx -g 'daemon off;'
